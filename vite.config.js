b494e28598c08d2e6142276a62e23be72d0b9dd7
Autri Baghkhanian <autri48@gmail.com>
2025-11-19 21:49:36 -0800

    Clean up vite comment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bug-butler/', // Change this to your GitHub repo name
+  base: '/bug-butler/',
 })
